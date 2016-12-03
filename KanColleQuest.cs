644b97cfc31be13f6643fcf451b53452e3547a7c
dongchenyu163 <dongchenyu163sohu@163.com>
2016-12-04 01:43:46 +0900

    基本截取实现
    TODO:合并单元格的处理

--- KanColleQuest.cs
+++ KanColleQuest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace KanColleQuestViewer
 {
 	class KanColleQuest
 	{
+		public static Collection<KanColleQuest> Quests = new Collection<KanColleQuest>();
 		/// <summary>
 		/// 任务编号
 		/// </summary>
-		QuestID ID;
+		string ID;
 		/// <summary>
 		/// 任务名称
 		/// </summary>
@@ -33,14 +35,136 @@ namespace KanColleQuestViewer
 		/// 开放条件是否确定
 		/// </summary>
 		bool isSure = true;
+
+		///// <summary>
+		///// 前置任务
+		///// </summary>
+		//Collection<KanColleQuest> preQuests;
+		///// <summary>
+		///// 后续会打开的任务
+		///// </summary>
+		//Collection<KanColleQuest> nextQuests;
+
+		KanColleQuest(string ID,
+					string NameOfQuest,
+					string DetailOfQuest,
+					Rewards Rewards,
+					string OtherInfoOfQuest)
+		{
+			this.ID = ID;
+			this.NameOfQuest = NameOfQuest;
+			this.DetailOfQuest = DetailOfQuest;
+			this.RewardOfQuest = Rewards;
+			this.OtherInfoOfQuest = OtherInfoOfQuest;
+		}
+
+		public static void CreateStringProcess(string str)
+		{
+			string _split_1_st = "<td>", _split_1_ed = "</td>";
+			string _split_2_st = "<span lang=\"ja\">", _split_2_ed = "</span>";
+			int _IndexSt = 0;
+			int _IndexEd = 0;
+
+			#region 1、取出 ID
+			_IndexEd = str.IndexOf("\"", 0);
+			string _ID = str.Substring(0, _IndexEd);
+			str = str.Substring(_IndexEd);
+
+			#endregion
+
+			#region 2、取出 任务名称
+			_IndexSt = str.IndexOf(_split_2_st, 0);//寻找st在src的位置，返回st第一个字符的位置。
+			_IndexSt += _split_2_st.Length;//开始位置变更至末尾
+			_IndexEd = str.IndexOf(_split_2_ed, _IndexSt);
+
+			string _Name = str.Substring(_IndexSt, _IndexEd-_IndexSt);
+			str = str.Substring(_IndexEd);
+
+			#endregion
+
+			#region 3、取出 任务的详细信息。需要额外的字符处理
+			//取出原始 详细信息 字串
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			string _Detail = str.Substring(_IndexSt, _IndexEd-_IndexSt);
+			str = str.Substring(_IndexEd);
+			#region 额外的字符处理
+			//TODO: 额外信息 字串 额外的字符处理
+
+			#endregion
+			#endregion
+
+			#region 4、取出报酬字串
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			int _Fu = Convert.ToInt32(str.Substring(_IndexSt, _IndexEd-_IndexSt));
+			str = str.Substring(_IndexEd);
+			/*****************************************************************/
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			int _Bu = Convert.ToInt32(str.Substring(_IndexSt, _IndexEd-_IndexSt));
+			str = str.Substring(_IndexEd);
+			/*****************************************************************/
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			int _Steel = Convert.ToInt32(str.Substring(_IndexSt, _IndexEd-_IndexSt));
+			str = str.Substring(_IndexEd);
+			/*****************************************************************/
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			int _Ba = Convert.ToInt32(str.Substring(_IndexSt, _IndexEd-_IndexSt));
+			str = str.Substring(_IndexEd);
+			/*****************************************************************/
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			string _OtherReward = str.Substring(_IndexSt, _IndexEd-_IndexSt);
+			str = str.Substring(_IndexEd);
+			#endregion
+
+			#region 5、取出额外信息：任务链信息。需要额外的字符处理
+			//取出原始 额外信息 字串
+			_IndexSt = str.IndexOf(_split_1_st, 0);
+			_IndexSt += _split_1_st.Length;
+			_IndexEd = str.IndexOf(_split_1_ed, _IndexSt);
+
+			string _OtherInfo = str.Substring(_IndexSt, _IndexEd-_IndexSt);
+			str = str.Substring(_IndexEd);
+			#region 额外的字符处理
+			//TODO: 额外信息 字串 额外的字符处理
+			#endregion
+			#endregion
+
+			//添加新的任务
+			Quests.Add(new KanColleQuest(
+										_ID, 
+										_Name, 
+										_Detail,
+										new Rewards(_Fu,_Bu,_Steel,_Ba,_OtherReward),
+										_OtherInfo));
+			
+		}
+
 		/// <summary>
-		/// 前置任务
+		/// 以ID寻找任务。
 		/// </summary>
-		KanColleQuest[] preQuests;
-		/// <summary>
-		/// 后续会打开的任务
-		/// </summary>
-		KanColleQuest[] nextQuests;
+		/// <param name="ID">任务ID字符串(不分大小写)</param>
+		/// <returns>KanColleQuest类型引用，没找到则为null。</returns>
+		public static KanColleQuest Find(string ID)
+		{
+			return null;
+		}
 	}
 
 	class QuestID
@@ -53,17 +177,31 @@ namespace KanColleQuestViewer
 		/// 任务序号
 		/// </summary>
 		int Num = 0;
+
+		QuestID(string data)
+		{
+
+		}
 	}
 
 	class Rewards
 	{
-		int Fuel = 0;
-		int Bullet = 0;
-		int Steel = 0;
-		int Bauxite = 0;
+		public int Fuel;
+		public int Bullet;
+		public int Steel;
+		public int Bauxite;
 		/// <summary>
 		/// 其他的报酬
 		/// </summary>
-		string OtherRewards = "";
+		string OtherRewards;
+
+		public Rewards(int Fu, int Bu, int Steel, int Ba, string Other)
+		{
+			Fuel = Fu;
+			Bullet = Bu;
+			this.Steel = Steel;
+			Bauxite = Ba;
+			OtherRewards = Other;
+		}
 	}
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,14 +20,89 @@ namespace KanColleQuestViewer
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public static string g_url= @"https://zh.moegirl.org/%E8%88%B0%E9%98%9FCollection/%E4%BB%BB%E5%8A%A1#.E6.94.B9.E8.A3.85";
-		public static string g_HTTP_Res;
+		public static string gs_url= @"https://zh.moegirl.org/%E8%88%B0%E9%98%9FCollection/%E4%BB%BB%E5%8A%A1#.E6.94.B9.E8.A3.85";
+		public static string gs_HTTP_Res;
 
+		public static string gs_Find_UpdateTime = ">此页面的内容及资料需要长期更新，现存条目中资料未必是最新。<li>本条目的<b>最后</b>更新时间为<b><u>";
+		public static string gs_Find_UpdateTime_end = "</u>";
+		string gs_Find_Quest = "<td id=\"";
+		string gs_Find_Quest_end = "</tr>";
+
+		public static string gs_UpdateTime = "";
+		
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			Tx_Res.Text = HTTP.GetHtmlContent(g_url,Encoding.UTF8);
+			gs_HTTP_Res = HTTP.GetHtmlContent(gs_url,Encoding.UTF8);
+
+			GetMidString(gs_HTTP_Res, ref gs_UpdateTime, gs_Find_Quest, gs_Find_Quest_end);
+			Tx_Res.Text = gs_UpdateTime;
+
+			HTML_StringProcess();
+		}
+
+		/// <summary>
+		/// 获取src中，在st ed字符串之间的字符串。
+		/// </summary>
+		/// <param name="src"></param>
+		/// <param name="res"></param>
+		/// <param name="st"></param>
+		/// <param name="ed"></param>
+		/// <returns></returns>
+		private bool GetMidString(string src,ref string res,string st,string ed,int startIndex=0)
+		{
+			int _IndexSt = -1;
+			int _IndexEd = -1;
+
+			_IndexSt = src.IndexOf(st, startIndex);//寻找st在src的位置，返回st第一个字符的位置。
+			_IndexSt += st.Length;//开始位置变更至st的末尾
+			_IndexEd = src.IndexOf(ed, _IndexSt);
+
+			if (_IndexSt == -1 || _IndexEd == -1)
+			{
+				res = "";
+				return false;
+			}
+			
+			res = src.Substring(_IndexSt, _IndexEd - _IndexSt);
+
+			return true;
+		}
+
+		private void HTML_StringProcess()
+		{
+			int _IndexSt = 0;
+			int _IndexEd = 0;
+			int _count = 0;
+			string _quest = "";
+			string _copyOf_gs_HTTP_Res = gs_HTTP_Res;
+			
+			// 获取更新日期
+			GetMidString(_copyOf_gs_HTTP_Res, 
+				ref gs_UpdateTime, 
+				gs_Find_UpdateTime, 
+				gs_Find_UpdateTime_end);
+
+			while (true)
+			{
+				_IndexSt = _copyOf_gs_HTTP_Res.IndexOf(gs_Find_Quest, 0);//寻找st在src的位置，返回st第一个字符的位置。
+				_IndexSt += gs_Find_Quest.Length;//开始位置变更至末尾
+				_IndexEd = _copyOf_gs_HTTP_Res.IndexOf(gs_Find_Quest_end, _IndexSt);
+
+				if(_IndexSt==-1|| _IndexEd==-1)
+				{
+					break;
+				}
+
+				_count++;
+
+				_quest= _copyOf_gs_HTTP_Res.Substring(_IndexSt, _IndexEd - _IndexSt);
+
+				KanColleQuest.CreateStringProcess(_quest);
+				//删除前面的字符。
+				_copyOf_gs_HTTP_Res=_copyOf_gs_HTTP_Res.Substring(_IndexEd);
+			}
 		}
 	}
 }
