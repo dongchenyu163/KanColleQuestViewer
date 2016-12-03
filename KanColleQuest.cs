0070b7e16eb471651a922a8a2749e3305a4a5d2b
dongchenyu163 <dongchenyu163sohu@163.com>
2016-12-03 19:06:24 +0900

    一小步
    完成HTTP；
    完成任务类成员、注释；

---
+++ KanColleQuest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanColleQuestViewer
+{
+	class KanColleQuest
+	{
+		/// <summary>
+		/// 任务编号
+		/// </summary>
+		QuestID ID;
+		/// <summary>
+		/// 任务名称
+		/// </summary>
+		string NameOfQuest;
+		/// <summary>
+		/// 任务的详细信息
+		/// </summary>
+		string DetailOfQuest;
+		/// <summary>
+		/// 获得的奖励
+		/// </summary>
+		Rewards RewardOfQuest;
+		/// <summary>
+		/// 任务的追加信息
+		/// </summary>
+		string OtherInfoOfQuest;
+
+		/// <summary>
+		/// 开放条件是否确定
+		/// </summary>
+		bool isSure = true;
+		/// <summary>
+		/// 前置任务
+		/// </summary>
+		KanColleQuest[] preQuests;
+		/// <summary>
+		/// 后续会打开的任务
+		/// </summary>
+		KanColleQuest[] nextQuests;
+	}
+
+	class QuestID
+	{
+		/// <summary>
+		/// 任务类型的字母代号
+		/// </summary>
+		string Alphabet = "A";
+		/// <summary>
+		/// 任务序号
+		/// </summary>
+		int Num = 0;
+	}
+
+	class Rewards
+	{
+		int Fuel = 0;
+		int Bullet = 0;
+		int Steel = 0;
+		int Bauxite = 0;
+		/// <summary>
+		/// 其他的报酬
+		/// </summary>
+		string OtherRewards = "";
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KanColleQuestViewer
+{
+	/// <summary>
+	/// MainWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public static string g_url= @"https://zh.moegirl.org/%E8%88%B0%E9%98%9FCollection/%E4%BB%BB%E5%8A%A1#.E6.94.B9.E8.A3.85";
+		public static string g_HTTP_Res;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			Tx_Res.Text = HTTP.GetHtmlContent(g_url,Encoding.UTF8);
+		}
+	}
+}
